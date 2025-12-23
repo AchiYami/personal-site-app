7250347c52e3224c2242a1b8c03c4968d74110a6
Simon Campbell <sjcampbell750@gmail.com>
2025-12-23 00:30:47 +0000

    wip: card component on page

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,8 @@
+﻿<script>
+    import Card from "../../components/card/card.svelte";
+</script>
+
+<button class="btn btn-primary">Test Button</button>
+
+<Card icon="mdi:home" iconHeight={24} iconWidth={24}>
+</Card>
